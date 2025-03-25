ec15219e835d8a2c8f7081fe503740f9a58000d5
rjbrown6 <Robert.J.Brown@nasa.gov>
2025-03-25 12:05:42 -0400

    Revert "style: auto-format via clang-format"
    This reverts commit fbbe71b13d23e011a77c37300c4a61856c0e6b53.

--- fsw/cfs/unit_test/cam_test_utils.c
+++ fsw/cfs/unit_test/cam_test_utils.c
@@ -1,21 +1,15 @@
 /*
-  Copyright (C) 2009 - 2016 National Aeronautics and Space Administration. All
-  Foreign Rights are Reserved to the U.S. Government.
+  Copyright (C) 2009 - 2016 National Aeronautics and Space Administration. All Foreign Rights are Reserved to the U.S. Government.
 
-  This software is provided "as is" without any warranty of any, kind either
-  express, implied, or statutory, including, but not limited to, any warranty
-  that the software will conform to, specifications any implied warranties of
-  merchantability, fitness for a particular purpose, and freedom from
-  infringement, and any warranty that the documentation will conform to the
-  program, or any warranty that the software will be error free.
+  This software is provided "as is" without any warranty of any, kind either express, implied, or statutory, including, but not
+  limited to, any warranty that the software will conform to, specifications any implied warranties of merchantability, fitness
+  for a particular purpose, and freedom from infringement, and any warranty that the documentation will conform to the program, or
+  any warranty that the software will be error free.
 
-  In no event shall NASA be liable for any damages, including, but not limited
-  to direct, indirect, special or consequential damages, arising out of,
-  resulting from, or in any way connected with the software or its
-  documentation.  Whether or not based upon warranty, contract, tort or
-  otherwise, and whether or not loss was sustained from, or arose out of the
-  results of, or use of, the software, documentation or services provided
-  hereunder
+  In no event shall NASA be liable for any damages, including, but not limited to direct, indirect, special or consequential damages,
+  arising out of, resulting from, or in any way connected with the software or its documentation.  Whether or not based upon warranty,
+  contract, tort or otherwise, and whether or not loss was sustained from, or arose out of the results of, or use of, the software,
+  documentation or services provided hereunder
 
   ITC Team
   NASA IV&V
@@ -28,19 +22,19 @@
 
 #include <i2c_hooks.h>
 
+#include <utassert.h>
 #include <ut_cfe_es_stubs.h>
 #include <ut_cfe_sb_stubs.h>
 #include <ut_osapi_stubs.h>
-#include <utassert.h>
 
 #ifdef __linux__
-#include <sys/stat.h>
-#include <sys/types.h>
+    #include <sys/stat.h>
+    #include <sys/types.h>
 #endif
 
 /* prototypes */
-static int i2c_transaction(int handle, uint8_t addr, void *txbuf, uint8_t txlen,
-                           void *rxbuf, uint8_t rxlen, uint16_t timeout);
+static int i2c_transaction(int handle, uint8_t addr, void* txbuf, uint8_t txlen,
+                           void* rxbuf, uint8_t rxlen, uint16_t timeout);
 
 /* i2c data */
 i2c_data_t i2c_data;
@@ -52,57 +46,64 @@ static i2c_hooks_t i2c_hooks = {
     .i2c_master_transaction_hook = i2c_transaction,
 };
 
-void CAM_Test_Setup(void) {
-  /* initialize services */
-  Ut_CFE_SB_Reset();
-  Ut_CFE_ES_Reset();
-  Ut_OSAPI_Reset();
+void CAM_Test_Setup(void)
+{
+    /* initialize services */
+    Ut_CFE_SB_Reset();
+    Ut_CFE_ES_Reset();
+    Ut_OSAPI_Reset();
 
-  /* initialize app data */
-  CFE_PSP_MemSet(&CAM_AppData, 0, sizeof(CAM_AppData_t));
+    /* initialize app data */
+    CFE_PSP_MemSet(&CAM_AppData, 0, sizeof(CAM_AppData_t));
 
-  /* set i2c hooks */
-  memset(&i2c_data, 0, sizeof(i2c_data_t));
-  i2c_read_index = 0;
-  i2c_data.retcode = E_NO_ERR;
-  set_i2c_hooks(&i2c_hooks);
+    /* set i2c hooks */
+    memset(&i2c_data, 0, sizeof(i2c_data_t));
+    i2c_read_index = 0;
+    i2c_data.retcode = E_NO_ERR;
+    set_i2c_hooks(&i2c_hooks);
 }
 
-void CAM_Test_TearDown(void) { set_i2c_hooks(NULL); }
+void CAM_Test_TearDown(void)
+{
+    set_i2c_hooks(NULL);
+}
+
 
 /* i2c transaction hook */
-static int i2c_transaction(int handle, uint8_t addr, void *txbuf, uint8_t txlen,
-                           void *rxbuf, uint8_t rxlen, uint16_t timeout) {
-  /* verify buffers */
-  if (txlen > 0)
-    UtAssert_True(txbuf != NULL, "i2c txbuf != NULL");
-  if (rxlen > 0)
-    UtAssert_True(rxbuf != NULL, "i2c rxbuf != NULL");
+static int i2c_transaction(int handle, uint8_t addr, void* txbuf, uint8_t txlen,
+                           void* rxbuf, uint8_t rxlen, uint16_t timeout)
+{
+    /* verify buffers */
+    if(txlen > 0) UtAssert_True(txbuf != NULL, "i2c txbuf != NULL");
+    if(rxlen > 0) UtAssert_True(rxbuf != NULL, "i2c rxbuf != NULL");
 
-  /* verify basic cam i2c params */
-  UtAssert_True(handle == CAM_I2C_HANDLE, "cam i2c handle");
-  UtAssert_True(addr == CAM_I2C_ADDRESS, "cam i2c address");
+    /* verify basic cam i2c params */
+    UtAssert_True(handle == CAM_I2C_HANDLE, "cam i2c handle");
+    UtAssert_True(addr == CAM_I2C_ADDRESS, "cam i2c address");
 
-  /* save tx data for testing */
-  if (txlen > 0) {
-    uint16_t avail = CAM_I2C_BUF_MAX - i2c_data.txlen;
-    UtAssert_True(txlen <= avail, "i2c txbuf overflow");
-    uint16_t len = (txlen > avail) ? avail : txlen;
-    memcpy(i2c_data.txbuf + i2c_data.txlen, txbuf, len);
-    i2c_data.txlen += len;
-  }
-
-  /* only process on no error */
-  if (i2c_data.retcode == E_NO_ERR) {
-    /* return rxbuf test data */
-    if (rxlen > 0) {
-      uint16_t avail = i2c_data.rxlen - i2c_read_index;
-      // UtAssert_True(rxlen <= avail, "i2c rxbuf underflow");
-      uint16_t len = (rxlen > avail) ? avail : rxlen;
-      memcpy(rxbuf, i2c_data.rxbuf + i2c_read_index, len);
-      i2c_read_index += len;
+    /* save tx data for testing */
+    if(txlen > 0)
+    {
+        uint16_t avail = CAM_I2C_BUF_MAX - i2c_data.txlen;
+        UtAssert_True(txlen <= avail, "i2c txbuf overflow");
+        uint16_t len = (txlen > avail) ? avail : txlen;
+        memcpy(i2c_data.txbuf + i2c_data.txlen, txbuf, len);
+        i2c_data.txlen += len;
     }
-  }
 
-  return i2c_data.retcode;
+    /* only process on no error */
+    if(i2c_data.retcode == E_NO_ERR)
+    {
+        /* return rxbuf test data */
+        if(rxlen > 0)
+        {
+            uint16_t avail = i2c_data.rxlen - i2c_read_index;
+            //UtAssert_True(rxlen <= avail, "i2c rxbuf underflow");
+            uint16_t len = (rxlen > avail) ? avail : rxlen;
+            memcpy(rxbuf, i2c_data.rxbuf + i2c_read_index, len);
+            i2c_read_index += len;
+        }
+    }
+
+    return i2c_data.retcode;
 }
--- fsw/fprime/src/cam_device.h
+++ fsw/fprime/src/cam_device.h
@@ -1,18 +1,13 @@
-/* Copyright (C) 2009 - 2017 National Aeronautics and Space Administration. All
-Foreign Rights are Reserved to the U.S. Government.
+/* Copyright (C) 2009 - 2017 National Aeronautics and Space Administration. All Foreign Rights are Reserved to the U.S. Government.
 
-This software is provided "as is" without any warranty of any, kind either
-express, implied, or statutory, including, but not limited to, any warranty that
-the software will conform to, specifications any implied warranties of
-merchantability, fitness for a particular purpose, and freedom from
-infringement, and any warranty that the documentation will conform to the
-program, or any warranty that the software will be error free.
+This software is provided "as is" without any warranty of any, kind either express, implied, or statutory, including, but not
+limited to, any warranty that the software will conform to, specifications any implied warranties of merchantability, fitness
+for a particular purpose, and freedom from infringement, and any warranty that the documentation will conform to the program, or
+any warranty that the software will be error free.
 
-In no event shall NASA be liable for any damages, including, but not limited to
-direct, indirect, special or consequential damages, arising out of, resulting
-from, or in any way connected with the software or its documentation.  Whether
-or not based upon warranty, contract, tort or otherwise, and whether or not loss
-was sustained from, or arose out of the results of, or use of, the software,
+In no event shall NASA be liable for any damages, including, but not limited to direct, indirect, special or consequential damages,
+arising out of, resulting from, or in any way connected with the software or its documentation.  Whether or not based upon warranty,
+contract, tort or otherwise, and whether or not loss was sustained from, or arose out of the results of, or use of, the software,
 documentation or services provided hereunder
 
 ITC Team
@@ -23,14 +18,14 @@ ivv-itc@lists.nasa.gov
 #ifndef _cam_device_h_
 #define _cam_device_h_
 
-#include "cam_app.h"
-#include "cam_events.h"
-#include "cam_msg.h"
-#include "cam_msgids.h"
-#include "cam_perfids.h"
-#include "cam_platform_cfg.h"
-#include "cam_version.h"
 #include "cfe.h"
+#include "cam_app.h"
+#include "cam_perfids.h"
+#include "cam_msgids.h"
+#include "cam_msg.h"
+#include "cam_events.h"
+#include "cam_version.h"
+#include "cam_platform_cfg.h"
 
 /************************************************************************
 ** Debug Definitions
@@ -41,55 +36,55 @@ ivv-itc@lists.nasa.gov
 /************************************************************************
 ** Type Definitions
 *************************************************************************/
-#define CAM_I2C_BUS 2
-#define CAM_SPEED 1000000
-#define CAP_DONE_MASK 0x08
-#define CAM_TIMEOUT 100
+#define CAM_I2C_BUS             2
+#define CAM_SPEED               1000000
+#define CAP_DONE_MASK           0x08
+#define CAM_TIMEOUT             100
 //#define CAM_DATA_SIZE           1010
 
 // Select Hardware (only 1)
-//#define OV2640
-#define OV5640
-//#define OV5642
+    //#define OV2640
+    #define OV5640 
+    //#define OV5642
 
 // Hardware Specific Definitions
 #ifdef OV2640
-#define CAM_ADDR 0x30
-#define CHIPID_HIGH 0x0A
-#define CHIPID_LOW 0x0B
-#define CAM_VID 0x26
-#define CAM_PID 0x42
-#define MAX_FIFO_SIZE 0x5FFFF // 384KByte
+    #define CAM_ADDR			0x30
+    #define CHIPID_HIGH         0x0A
+    #define CHIPID_LOW          0x0B
+    #define CAM_VID             0x26
+    #define CAM_PID             0x42
+    #define MAX_FIFO_SIZE       0x5FFFF // 384KByte
 #endif
 #ifdef OV5640
-#define CAM_ADDR 0x3C
-#define CHIPID_HIGH 0x300A
-#define CHIPID_LOW 0x300B
-#define CAM_VID 0x56
-#define CAM_PID 0x40
-#define MAX_FIFO_SIZE 0x7FFFFF // 8MByte
+    #define CAM_ADDR	        0x3C
+    #define CHIPID_HIGH         0x300A
+    #define CHIPID_LOW          0x300B
+    #define CAM_VID             0x56
+    #define CAM_PID             0x40
+    #define MAX_FIFO_SIZE       0x7FFFFF // 8MByte
 #endif
 #ifdef OV5642
-#define CAM_ADDR 0x3C
-#define CHIPID_HIGH 0x300A
-#define CHIPID_LOW 0x300B
-#define CAM_VID 0x56
-#define CAM_PID 0x42
-#define MAX_FIFO_SIZE 0x7FFFFF // 8MByte
+    #define CAM_ADDR	        0x3C
+    #define CHIPID_HIGH         0x300A
+    #define CHIPID_LOW          0x300B
+    #define CAM_VID             0x56
+    #define CAM_PID             0x42
+    #define MAX_FIFO_SIZE       0x7FFFFF // 8MByte
 #endif
 
-#define size_160x120 0
-#define size_320x240 1
-#define size_800x600 2
-#define size_1600x1200 3
-#define size_2592x1944 4
+#define size_160x120			0
+#define size_320x240			1
+#define size_800x600			2
+#define size_1600x1200			3
+#define size_2592x1944          4
 
 /*************************************************************************
 ** Structures
 *************************************************************************/
 struct sensor_reg {
-  uint16_t reg;
-  uint16_t val;
+    uint16_t reg;
+    uint16_t val;
 };
 
 /*************************************************************************
@@ -107,9 +102,10 @@ extern int32 CAM_setup(void);
 extern int32 CAM_setSize(uint8_t size);
 extern int32 CAM_capture_prep(void);
 extern int32 CAM_capture(void);
-extern int32 CAM_read_fifo_length(uint32 *length);
-extern int32 CAM_read_prep(char *buf, uint16 *i);
-extern int32 CAM_read(char *buf, uint16 *i, uint8 *status);
+extern int32 CAM_read_fifo_length(uint32* length);
+extern int32 CAM_read_prep(char* buf, uint16* i);
+extern int32 CAM_read(char* buf, uint16* i, uint8* status);
+
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 /*                                                                 */
@@ -118,14 +114,15 @@ extern int32 CAM_read(char *buf, uint16 *i, uint8 *status);
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 int32 CAM_ChildInit(void);
-void CAM_ChildTask(void);
+void  CAM_ChildTask(void);
 int32 CAM_publish(void);
 int32 CAM_state(void);
-int32 CAM_fifo(uint16 *, uint8 *);
-int32 CAM_exp(void);
+int32 CAM_fifo(uint16*, uint8*);
+int32 CAM_exp(void); 
 
 #endif /* _cam_device_h_ */
 
 /************************/
 /*  End of File Comment */
 /************************/
+
